be50506ad5f746141a47579df25993b7ccdb1f1b
Natlia <napolu@yandex.ru>
2022-04-17 23:07:05 +0300

    method version i can understand

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int ShowNumber()
+{
+    int num = new Random().Next(10, 99);
+    int firstNum = num/10;
+    int secondNum = num%10;
+
+    Console.Write(num + " -> ");
+
+    if(firstNum > secondNum)
+    {
+        return firstNum;
+    }
+    else
+    {
+        return secondNum;
+    }
+}
+
+int a = ShowNumber();
+Console.WriteLine(a);
