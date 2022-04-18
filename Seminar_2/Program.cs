91274b173a03c29fe6f1a7e27b129ea722ff3176
Natlia <napolu@yandex.ru>
2022-04-18 19:35:24 +0300

    method version advanced

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -1,10 +1,9 @@
-﻿int ShowNumber()
+﻿int ShowNumber(int n)
 {
-    int num = new Random().Next(10, 99);
-    int firstNum = num/10;
-    int secondNum = num%10;
+    int firstNum = n/10;
+    int secondNum = n%10;
 
-    Console.Write(num + " -> ");
+    Console.Write(n + " -> ");
 
     if(firstNum > secondNum)
     {
@@ -16,5 +15,6 @@
     }
 }
 
-int a = ShowNumber();
+int num = new Random().Next(10, 99);
+int a = ShowNumber(num);
 Console.WriteLine(a);
